98f1e057586983f8a9374766c2c334bdaba2507e
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-07 17:30:00 -0500

    frames implemented as hashmap, change every other turn

--- Test/com/nicole/bowling/test/GameTest.java
+++ Test/com/nicole/bowling/test/GameTest.java
@@ -1,7 +1,43 @@
 package com.nicole.bowling.test;
+import com.nicole.bowling.Game;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Created by bears8yourface on 1/7/16.
  */
-public class GameTest {
+public class GameTest extends TestCase {
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    public void testGutterGame() {
+        for (int i=0; i<20; i++)
+            game.roll(0);
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void testAllOnes() {
+        for (int i=0; i<20; i++)
+            game.roll(1);
+        assertEquals(20, game.score());
+    }
+
+//    @Test
+//    public void testASpare() {
+//        game.roll(9);
+//        game.roll(1);
+//        game.roll(3);
+//        for (int i=0; i<17; i++)
+//            game.roll(0);
+//
+//        assertEquals(16, game.score());
+//    }
 }
--- src/com/nicole/bowling/Game.java
+++ src/com/nicole/bowling/Game.java
@@ -1,7 +1,64 @@
 package com.nicole.bowling;
 
+import java.util.HashMap;
+
 /**
  * Created by bears8yourface on 1/7/16.
  */
 public class Game {
+    private int score;
+    private HashMap<Integer, int[]> frames;
+    private int currentFrame;
+    private int currentRoll;
+
+    public Game() {
+        score = 0;
+        currentFrame = 1;
+        currentRoll = 0;
+        frames = setupFrames();
+    }
+
+    public void roll(int numOfPins) {
+        score += numOfPins;
+
+        int[] thisFrame = frames.get(currentFrame);
+        System.out.print(thisFrame[currentRoll]);
+        thisFrame[currentRoll] = numOfPins;
+
+        if (frameIsComplete()) {
+            currentFrame += 1;
+        }
+
+        toggleCurrentRoll();
+    }
+
+    private Boolean frameIsComplete() {
+        int[] rolls = frames.get(currentFrame);
+        int frameScore = rolls[0] + rolls[1];
+        Boolean rolledTwice = currentRoll == 1; // FIND BETTER SOLUTION
+
+        return frameScore == 10 || rolledTwice;
+    }
+
+    public int score() {
+        return score;
+    }
+
+
+    private void toggleCurrentRoll() {
+        if (currentRoll == 0) {
+            currentRoll = 1;
+        } else {
+            currentRoll = 0;
+        }
+    }
+
+    private HashMap setupFrames() {
+        HashMap framesIncubator = new HashMap<Integer, int[]>();
+        for (int i = 1; i < 11; i++) {
+            int[] rolls = new int[2];
+            framesIncubator.put(i, rolls);
+        }
+        return framesIncubator;
+    }
 }
